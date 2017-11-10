451514757607478e66b9f2029dee76660ded192d
OsanBeuth <32953212+OsanBeuth@users.noreply.github.com>
2017-11-10 15:42:20 +0100

    Aufgabe 2B

---
+++ src/test/java/com/businessapp/TestSuite.java
@@ -0,0 +1,19 @@
+
+package com.businessapp;
+
+import com.businessapp.customer.IndividualCustomerTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   AppTest.class,
+   IndividualCustomerTests.class
+})
+
+public class TestSuite 
+{
+    
+}  
---
+++ src/test/java/com/businessapp/customer/IndividualCustomerTests.java
@@ -0,0 +1,65 @@
+package com.businessapp.customer;
+import org.junit.Test;
+import com.businessapp.model.IndividualCustomer;
+import java.util.Date;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class IndividualCustomerTests
+{
+    IndividualCustomer person = new IndividualCustomer();
+    
+    @Test
+    public void testFirstName()
+    {
+        person.setFirstName("Osan");
+        assertEquals("Osan", person.getFirstName());
+        
+        person.setId("");
+        assertEquals("", person.getId());
+        
+        person.setId(null);
+        assertEquals(null, person.getId());
+    }
+    
+    @Test
+    public void testName()
+    {
+        person.setName("Flüssigseife");
+        assertEquals("Flüssigseife", person.getName());
+        
+        person.setId("");
+        assertEquals("", person.getId());
+        
+        person.setId(null);
+        assertEquals(null, person.getId());
+    }
+    
+    
+    @Test
+    public void testid()
+    {
+        person.setId("2729930");
+        assertEquals("2729930", person.getId());
+        
+        person.setId("");
+        assertEquals("", person.getId());
+        
+        person.setId(null);
+        assertEquals(null, person.getId());
+
+        
+    }
+    
+    
+    @Test
+    public void testcreated()
+    {
+        person.setCreated(new Date(2017,12,12));
+        assertEquals(new Date(2017, 12, 12), person.getCreated());
+        
+        person.setId(null);
+        assertEquals(null, person.getId());
+    }
+    
+}
